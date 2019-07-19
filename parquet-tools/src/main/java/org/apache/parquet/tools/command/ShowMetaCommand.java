bf91ef7f5467129250b91977b02c548d9ea7646c
Sam Hendley <HendleyS@valassis.com>
2019-07-19 10:47:16 -0400

    added `-j` and `-m` options to showMeta command to output JSON formatted data

--- parquet-tools/src/main/java/org/apache/parquet/tools/command/ShowMetaCommand.java
+++ parquet-tools/src/main/java/org/apache/parquet/tools/command/ShowMetaCommand.java
@@ -25,26 +25,39 @@ import org.apache.commons.cli.Options;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
-
 import org.apache.parquet.hadoop.Footer;
 import org.apache.parquet.hadoop.ParquetFileReader;
+import org.apache.parquet.hadoop.metadata.FileMetaData;
+import org.apache.parquet.hadoop.metadata.ParquetMetadata;
+import org.apache.parquet.tools.Main;
 import org.apache.parquet.tools.util.PrettyPrintWriter;
 import org.apache.parquet.tools.util.PrettyPrintWriter.WhiteSpaceHandler;
 
+import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.List;
 
 public class ShowMetaCommand extends ArgsOnlyCommand {
-  public static final String[] USAGE = new String[] {
+  public static final String[] USAGE = new String[]{
     "<input>",
     "where <input> is the parquet file to print to stdout"
   };
 
   public static final Options OPTIONS;
+
   static {
     OPTIONS = new Options();
     Option originalType = OptionBuilder.withLongOpt("originalType")
       .withDescription("Print logical types in OriginalType representation.")
       .create('o');
+    Option json = OptionBuilder.withLongOpt("json")
+      .withDescription("Display meta in json")
+      .create('j');
+    Option multiline = OptionBuilder.withLongOpt("multiline")
+      .withDescription("Make the JSON output multiline")
+      .create('m');
+    OPTIONS.addOption(json);
+    OPTIONS.addOption(multiline);
     OPTIONS.addOption(originalType);
   }
 
@@ -73,23 +86,38 @@ public class ShowMetaCommand extends ArgsOnlyCommand {
 
     String[] args = options.getArgs();
     String input = args[0];
-    boolean showOriginalTypes = options.hasOption('o');
 
     Configuration conf = new Configuration();
     Path inputPath = new Path(input);
     FileStatus inputFileStatus = inputPath.getFileSystem(conf).getFileStatus(inputPath);
     List<Footer> footers = ParquetFileReader.readFooters(conf, inputFileStatus, false);
 
-    PrettyPrintWriter out = PrettyPrintWriter.stdoutPrettyPrinter()
-                                             .withAutoColumn()
-                                             .withWhitespaceHandler(WhiteSpaceHandler.COLLAPSE_WHITESPACE)
-                                             .withColumnPadding(1)
-                                             .build();
-
-    for(Footer f: footers) {
-      out.format("file: %s%n" , f.getFile());
-      MetadataUtils.showDetails(out, f.getParquetMetadata(), showOriginalTypes);
-      out.flushColumns();
+    if (options.hasOption('j')) {
+      PrintWriter writer = new PrintWriter(Main.out, true);
+      for (Footer f : footers) {
+        FileMetaData fd = f.getParquetMetadata().getFileMetaData();
+        //  keyValueMetaData is stored as JSON blob and the escaping is problematic so we exclude this field
+        HashMap<String, String> keyValueMetaData = new HashMap<>();
+        ParquetMetadata parquetMetadata = new ParquetMetadata(new FileMetaData(fd.getSchema(), keyValueMetaData, fd.getCreatedBy()), f.getParquetMetadata().getBlocks());
+        if (options.hasOption('m')) {
+          writer.println(ParquetMetadata.toPrettyJSON(parquetMetadata));
+        } else {
+          writer.println(ParquetMetadata.toJSON(parquetMetadata));
+        }
+      }
+      writer.println();
+    } else {
+      boolean showOriginalTypes = options.hasOption('o');
+      PrettyPrintWriter out = PrettyPrintWriter.stdoutPrettyPrinter()
+        .withAutoColumn()
+        .withWhitespaceHandler(WhiteSpaceHandler.COLLAPSE_WHITESPACE)
+        .withColumnPadding(1)
+        .build();
+      for (Footer f : footers) {
+        out.format("file: %s%n", f.getFile());
+        MetadataUtils.showDetails(out, f.getParquetMetadata(), showOriginalTypes);
+        out.flushColumns();
+      }
     }
   }
 }
